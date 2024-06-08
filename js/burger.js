d33096394db140da35b47b80e4053a1abe6bf576
nexorqk <pkms2a10@gmail.com>
2024-06-08 17:16:24 +0300

    refactor: webstorm help

--- js/burger.js
+++ js/burger.js
@@ -1,22 +1,22 @@
 export const burger = () => {
-  const burgerButton = document.querySelector(".burger");
-  const burgerMenu = document.querySelector(".burger__nav");
-  const burgerLink = document.querySelectorAll(".burger__nav-link");
+    const burgerButton = document.querySelector(".burger");
+    const burgerMenu = document.querySelector(".burger__nav");
+    const burgerLink = document.querySelectorAll(".burger__nav-link");
 
-  burgerButton.addEventListener("click", () => {
-    burgerButton.classList.toggle("opened");
-    burgerMenu.classList.toggle("visible");
-  });
+    burgerButton.addEventListener("click", () => {
+        burgerButton.classList.toggle("opened");
+        burgerMenu.classList.toggle("visible");
+    });
 
-  document.addEventListener("click", (e) => {
-    if (!burgerMenu.contains(e.target) && !burgerButton.contains(e.target)) {
-      burgerMenu.classList.remove("visible");
-    }
-  });
+    document.addEventListener("click", (e) => {
+        if (!burgerMenu.contains(e.target) && !burgerButton.contains(e.target)) {
+            burgerMenu.classList.remove("visible");
+        }
+    });
 
-  [...burgerLink].forEach((link) =>
-    link.addEventListener("click", () => {
-      burgerMenu.classList.remove("visible");
-    })
-  );
+    [...burgerLink].forEach((link) =>
+        link.addEventListener("click", () => {
+            burgerMenu.classList.remove("visible");
+        })
+    );
 };
